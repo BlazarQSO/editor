5194b673762820fba83a0304ff6ae3767ec8e7d8
BlazarQSO <BlazarQSO@gmail.com>
2020-10-13 00:12:09 +0300

    feat: create functions get and set cursor position

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,7 +5,7 @@ import ButtonStyle from '../Buttons/ButtonStyle';
 class Main extends React.Component {
     constructor(props) {
         super(props);
-
+        this.position = 0;
         this.state = {
             buttons: [
                 { active: false, className: 'bold', value: 'B', },
@@ -21,6 +21,9 @@ class Main extends React.Component {
         }
         this.target = '';
         this.keyDow = this.keyDow.bind(this);
+        this.getCursorPosition = this.getCursorPosition.bind(this);
+        this.setCursorPosition = this.setCursorPosition.bind(this);
+        this.findOutCurElement = this.findOutCurElement.bind(this);
     }
 
     findOutCurElement() {
@@ -35,12 +38,36 @@ class Main extends React.Component {
         }
     }
 
-    // click(e) {
-    //     this.target = e.target;
-    //     if (this.target.tagName === 'SPAN') {
-    //         this.setState({ classes: [...this.target.classList] });
-    //     }
-    // }
+    click(e) {
+        this.editor = document.querySelector('.editor');
+        // this.target = e.target;
+        // if (this.target.tagName === 'SPAN') {
+        //     this.setState({ classes: [...this.target.classList] });
+        // }
+    }
+
+    getCursorPosition(parent) {
+        let selection = document.getSelection()
+        let range = new Range
+        range.setStart(parent, 0)
+        range.setEnd(selection.anchorNode, selection.anchorOffset)
+        return range.toString().length
+    }
+
+    setCursorPosition(parent, position) {
+        let child = parent.firstChild
+        while (position > 0) {
+            let length = child.textContent.length
+            if (position > length) {
+                position -= length
+                child = child.nextSibling
+            }
+            else {
+                if (child.nodeType == 3) return document.getSelection().collapse(child, position)
+                child = child.firstChild
+            }
+        }
+    }
 
     getCaretPosition() {
         let caretOffset = 0;
@@ -60,22 +87,34 @@ class Main extends React.Component {
             }
             return btn;
         });
+        this.editor.focus();
+        this.setCursorPosition(this.editor, this.position);
         this.setState({ buttons });
     }
 
     keyDow(e) {
         const position = this.getCaretPosition();
         const selected = document.getSelection().toString().length;
-        const classes = this.state.classes.join(' ');
+        const classes = this.state.classes.sort().join(' ');
+
+        // const elem = document.querySelector('.editor');
+        const pos2 = this.getCursorPosition(this.editor);
+        console.log(position);
+        console.log(pos2);
 
         if (selected === 0) {
+            const isNewTextStyle = [...this.target.classList].sort().join() !== classes;
+            const newSpan = `<span class="${classes}">${e.key}</span>`;
             if (this.target.tagName !== 'SPAN') {
                 // const span = document.createElement('span');
                 // span.className = classes;
                 // span.textContent = e.key;
-                this.target.innerHTML = `<span class="${classes}">${e.key}</span>`
+                this.target.innerHTML = newSpan;
                 // span.focus();
                 e.preventDefault();
+            } else if (isNewTextStyle) {
+                let content = this.target.innerHTML;
+                this.target.innerHTML = content.slice(0, position) + newSpan + content.slice(position);
             }
         }
     }
@@ -98,12 +137,12 @@ class Main extends React.Component {
                     aria-label="rdw-editor"
                     contentEditable="true"
                     role="textbox"
-                    // onClick={this.click.bind(this)}
+                    onClick={this.click.bind(this)}
                     onKeyDown={this.keyDow.bind(this)}
                     onSelect={this.findOutCurElement.bind(this)}
                 >
-                    <div><span className="">...</span></div>
-                    {/* <div><span className="italic">italic</span></div> */}
+                    <div><span className="bold">123456789</span></div>
+                    <div><span className="italic">italic</span></div>
                 </div>
             </main>
         )
